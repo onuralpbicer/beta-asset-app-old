73427d197c5fb0a4e1ff94fe68f2d520211a3521
Onur Alp Bicer <onuralpbicer@gmail.com>
2023-03-18 00:27:17 +1100

    Fixed api return

--- apps/api/src/routes/equipment-types/getEquipmentTypes.ts
+++ apps/api/src/routes/equipment-types/getEquipmentTypes.ts
@@ -4,17 +4,15 @@ import { IStateMiddleware } from '../../shared/types'
 const getEquipmentTypes: IStateMiddleware = async (ctx) => {
     const { org_id } = ctx.state
 
-    ctx.body = {
-        test: await prisma.equipment_Types.findMany({
-            select: {
-                id: true,
-                name: true,
-            },
-            where: {
-                org_id,
-            },
-        }),
-    }
+    ctx.body = await prisma.equipment_Types.findMany({
+        select: {
+            id: true,
+            name: true,
+        },
+        where: {
+            org_id,
+        },
+    })
 }
 
 export default getEquipmentTypes
