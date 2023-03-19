b95f9bfd4d376b5c3e082d904550dfee4ac8926b
Onur Alp Bicer <onuralpbicer@gmail.com>
2023-03-19 22:10:08 +1100

    Added service to module

--- apps/app/src/app/maintenances/maintenances.module.ts
+++ apps/app/src/app/maintenances/maintenances.module.ts
@@ -4,6 +4,7 @@ import { MAINTENANCES_FEATURE_KEY, reducer } from './maintenances.reducer'
 import { StoreModule } from '@ngrx/store'
 import { EffectsModule } from '@ngrx/effects'
 import { MaintenancesEffects } from './maintenances.effects'
+import { MaintenanecesService } from './maintenances.service'
 
 @NgModule({
     declarations: [],
@@ -12,5 +13,6 @@ import { MaintenancesEffects } from './maintenances.effects'
         StoreModule.forFeature(MAINTENANCES_FEATURE_KEY, reducer),
         EffectsModule.forFeature([MaintenancesEffects]),
     ],
+    providers: [MaintenanecesService],
 })
 export class MaintenancesModule {}
